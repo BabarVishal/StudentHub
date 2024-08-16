a781dd761c9956673534b6f584e1c4996a9c2843
vishal <vishalbabar7351@gmail.com>
2024-08-16 20:51:37 +0530

    updeted my css code

--- routes/blog.js
+++ routes/blog.js
@@ -27,6 +27,7 @@ router.get("/add-new", (req, res) => {
 
 router.get("/:id", async (req, res) => {
   const blog = await Blog.findById(req.params.id).populate("createdBy");
+  console.log( "blogs deta :",blog);
   const comments = await Comment.find({ blogId: req.params.id }).populate("createdBy");
 
   return res.render("blog", {
